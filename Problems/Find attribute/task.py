1542fe2177b6b124d81af8b9fce2defc9617a4a2
codewithgsp <codewithgsp@gmail.com>
2020-07-19 16:15:42 +0200

    find attribute

--- Problems/Find attribute/task.py
+++ Problems/Find attribute/task.py
@@ -1,1 +1,8 @@
 from lxml import etree
+
+# xml_str = '<a attr="123"><b>hello</b><c/></a>'
+# xml_str = '<root a1="aba" a2="caba"/>'
+root = etree.fromstring(input())
+attr = input()
+
+print(root.get(attr) if attr in root.keys() else None)
